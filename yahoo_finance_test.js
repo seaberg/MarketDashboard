d94b291035d82cfad2bbc08b48448fda55007043
Jonas Sjöberg <jonas@seaberg.se>
2016-10-06 17:49:24 +0000

    Initial commit

---
+++ yahoo_finance_test.js
@@ -0,0 +1,9 @@
+var yahooFinance = require('./node_modules/yahoo-finance');
+
+yahooFinance.historical({
+  symbol: 'AAPL',
+  from: '2016-09-01',
+  to: '2016-09-30'
+}, function (err, quotes) {
+   console.log(quotes);
+});
